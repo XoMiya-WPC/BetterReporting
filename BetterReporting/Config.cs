97cced1ea224ea18b173864dfef1dffe5b027f2a
XoMiya-WPC <inspectorxomiya@gmail.com>
2021-09-12 13:23:20 +0100

    Uploading V1
    Uploading all files

---
+++ BetterReporting/Config.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using Exiled.API.Interfaces;
+
+namespace BetterReporting
+{
+    public class Config : IConfig
+    {
+        [Description("Whether or not the plugin is enabled.")]
+        public bool IsEnabled { get; set; } = true;
+        [Description("Whether or not to override the server name with the Server_Name Config Value - Accepts Bool.")]
+        public bool OverrideName { get; set; } = false;
+        [Description("Defines the overriden server name - Acepts String.")]
+        public string ServerName { get; set; }
+        [Description("List of role IDs that will be pinged when a Local Report is sent. - Accepts String (Example '<@&modid> <@&adminid>').")]
+        public string LocalRoleIds { get; set; }
+        [Description("List of role IDs that will be pinged when a Cheater Report is sent. - Accepts String (Example '<@&modid> <@&adminid>').")]
+        public string CheaterRoleIds { get; set; }
+        [Description("List of role IDs that will be pinged if a user with remote admin authentication is reported. This role should probably mention some management or something. - Accepts String (Example '<@&managerid> <@&ownerid> ').")]
+        public string ManagementRoleIds { get; set; }
+        [Description("Your Webhook URL from Discord. Copy and paste it in its entirity.")]
+        public string Webhook { get; set; } = "https://discord.com/api/webhooks/XXXX/XXXX";
+        [Description("A Custom Message that will send with the webhook alongside the role mentions. This is a string and can work with discord formatting i.e ('A new **Report** has been summitted :pencil:').")]
+        public string CustomMessage { get; set; } = "A new **Report** has been summitted. :pencil:";
+        [Description("You can visit this website for help converting your colours: https://www.mathsisfun.com/hexadecimal-decimal-colors.html. This is the custom DECIMAL colour of your embed for Local Reports. If you have a hex colour code, please convert it first to decimal. - Accepts Decimal")]
+        public int LocalReportColor { get; set; } = 255;
+        [Description("This is the custom DECIMAL colour of your embed for Cheater Reports. If you have a hex colour code, please convert it first to decimal. - Accepts Decimal")]
+        public int CheaterReportColor { get; set; } = 16711680;
+        [Description("Message sent to ingame staff when a report is submitted. - Accepts String")]
+        public string AcMessage { get; set; } = "A new report has been submitted. - Check Discord";
+        [Description("Verbose mode. Prints more Debug console messages.")]
+        public bool VerboseMode { get; set; } = false;
+    }
+}
---
+++ BetterReporting/Handlers/ServerEventHandlers.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Net.Http;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using Utf8Json;
+
+namespace BetterReporting.Handlers
+{
+    public class ServerEventHandlers
+    {
+        private readonly Plugin plugin;
+        public ServerEventHandlers(Plugin plugin) => this.plugin = plugin;
+
+        public void CheaterReport(ReportingCheaterEventArgs ev)
+        {
+            //Check for and prevent self reports.
+            if (ev.Reported.UserId == ev.Reporter.UserId)
+            {
+                ev.Reporter.Broadcast(5, "You cannot report yourself!");
+                return;
+            }
+            //Check for and prevent reporting of Northwood Staff.
+            else if (ev.Reported.IsNorthwoodStaff)
+            {
+                ev.Reporter.Broadcast(5, "You cannot report Northwood Staff!");
+                return;
+            }
+            var CheaterRoleIdsString = "";
+            if (ev.Reported.RemoteAdminAccess)
+                CheaterRoleIdsString = plugin.Config.CheaterRoleIds + " " + plugin.Config.ManagementRoleIds;
+            else if (!ev.Reported.RemoteAdminAccess)
+                CheaterRoleIdsString = plugin.Config.CheaterRoleIds;
+            Log.Info("[FLAG] - Warning a user with Remote Admin Authentication has been reported to Northwood for Cheating.");
+            string ReporterRole = Convert.ToString(ev.Reporter.Role);
+            string ReportedRole = Convert.ToString(ev.Reported.Role);
+            if (ReporterRole == "None")
+                ReporterRole = "Spectator";
+            if (ReportedRole == "None")
+                ReportedRole = "Spectator";
+            var ServerNameVal = "";
+            if (plugin.Config.OverrideName)
+                ServerNameVal = plugin.Config.ServerName;
+            else if (!plugin.Config.OverrideName)
+                ServerNameVal = StripRichText(GameCore.ConfigFile.ServerConfig.GetString("server_name"));
+            var webHook = new
+            {
+                content = plugin.Config.CustomMessage + "\n" + CheaterRoleIdsString,
+                embeds = new List<object>
+                {
+                    new
+                    {
+                        title = "New In-game report (Cheater)",
+                        color = plugin.Config.CheaterReportColor,
+                        fields = new List<object>
+                        {
+                            new
+                            {
+                                name = "Server Name",
+                                value = ServerNameVal,
+                            },
+                            new
+                            {
+                                name = "Server Endpoint",
+                                value = Server.IpAddress+":"+Server.Port,
+                            },
+                            new
+                            {
+                                name = "Reporter Nickname",
+                                value = ev.Reporter.Nickname,
+                            },
+                            new
+                            {
+                                name = "Reporter User ID",
+                                value = "`"+ev.Reporter.UserId+"`",
+                            },
+                            new
+                            {
+                                name = "Reporter Class",
+                                value = ReporterRole,
+                            },
+                            new
+                            {
+                                name = "Reported Player Nickname",
+                                value = ev.Reported.Nickname,
+                            },
+                            new
+                            {
+                                name = "Reported Player User ID",
+                                value = "`"+ev.Reported.UserId+"`",
+                            },
+                            new
+                            {
+                                name = "Reported Player ID",
+                                value = "`"+ev.Reported.Id+"`",
+                            },
+                            new
+                            {
+                                name = "Reported Class",
+                                value = ReportedRole,
+                                inline = true,
+                            },
+                            new
+                            {
+                                name = "Report Reason",
+                                value = ev.Reason,
+                            }
+                        },
+                        timestamp = DateTime.UtcNow.ToString("o")
+                    }
+                }
+            };
+            if (plugin.Config.VerboseMode)
+                Log.Debug($"A new cheater report has been made. Sending report to Discord... JSON: {Encoding.UTF8.GetString(JsonSerializer.Serialize<object>(webHook))}");
+            ev.Reporter.Broadcast(5, "Thank you for submitting a Cheater Report.");
+            foreach (var user in Player.List)
+            {
+                if (user.ReferenceHub.serverRoles.AdminChatPerms)
+                {
+                    user.Broadcast(10, plugin.Config.AcMessage, Broadcast.BroadcastFlags.AdminChat);
+                    if (plugin.Config.VerboseMode)
+                    {
+                        Log.Debug($"Sent Admin Chat alerting to report to: {user.Nickname}");
+                    }
+                }
+            }
+            if (ev.Reported.RemoteAdminAccess)
+                Log.Info("Attention - A Staff Member was Reported!");
+            StringContent reportStringContent = new StringContent(Encoding.UTF8.GetString(JsonSerializer.Serialize<object>(webHook)), Encoding.UTF8, "application/json");
+            _ = plugin.HttpHandler.Send(plugin.Config.Webhook, reportStringContent);
+        }
+        public void LocalReport(LocalReportingEventArgs ev)
+        {
+            //Check for and prevent self reports.
+            if (ev.Target.UserId == ev.Issuer.UserId)
+            {
+                ev.Issuer.Broadcast(5, "You cannot report yourself!");
+                return;
+            }
+            //Check for and prevent reporting of Northwood Staff.
+            else if (ev.Target.IsNorthwoodStaff)
+            {
+                ev.Issuer.Broadcast(5, "You cannot report Northwood Staff!");
+                return;
+            }
+            var LocalRoleIdsString = "";
+            if (ev.Target.RemoteAdminAccess)
+                LocalRoleIdsString = plugin.Config.LocalRoleIds + " " + plugin.Config.ManagementRoleIds;
+            else if (!ev.Target.RemoteAdminAccess)
+                LocalRoleIdsString = plugin.Config.LocalRoleIds;
+            string ReporterRole = Convert.ToString(ev.Issuer.Role);
+            string ReportedRole = Convert.ToString(ev.Target.Role);
+            if (ReporterRole == "None")
+                ReporterRole = "Spectator";
+            if (ReportedRole == "None")
+                ReportedRole = "Spectator";
+            var ServerNameVal = "";
+            if (plugin.Config.OverrideName)
+                ServerNameVal = plugin.Config.ServerName;
+            else if (!plugin.Config.OverrideName)
+                ServerNameVal = StripRichText(GameCore.ConfigFile.ServerConfig.GetString("server_name"));
+            var webHook = new
+            {
+                content = plugin.Config.CustomMessage + "\n" + LocalRoleIdsString,
+                embeds = new List<object>
+                {
+                    new
+                    {
+                        title = "New In-game Report (Rule Breaking)",
+                        color = plugin.Config.LocalReportColor,
+                        fields = new List<object>
+                        {
+                            new
+                            {
+                                name = "Server Name",
+                                value = ServerNameVal,
+                            },
+                            new
+                            {
+                                name = "Server Endpoint",
+                                value = Server.IpAddress+":"+Server.Port,
+                            },
+                            new
+                            {
+                                name = "Reporter Nickname",
+                                value = ev.Issuer.Nickname,
+                            },
+                            new
+                            {
+                                name = "Reporter User ID",
+                                value = "`"+ev.Issuer.UserId+"`",
+                            },
+                            new
+                            {
+                                name = "Reporter Class",
+                                value = ReporterRole,
+                            },
+                            new
+                            {
+                                name = "Reported Player Nickname",
+                                value = ev.Target.Nickname,
+                            },
+                            new
+                            {
+                                name = "Reported Player User ID",
+                                value = "`"+ev.Target.UserId+"`",
+                            },
+                            new
+                            {
+                                name = "Reported Player ID",
+                                value = "`"+ev.Target.Id+"`",
+                            },
+                            new
+                            {
+                                name = "Reported Class",
+                                value = ReportedRole,
+                                inline = true,
+                            },
+                            new
+                            {
+                                name = "Report Reason",
+                                value = ev.Reason,
+                            }
+                        },
+                        timestamp = DateTime.UtcNow.ToString("o")
+                    }
+                }
+            };
+            if (plugin.Config.VerboseMode)
+                Log.Debug($"A new local report has been made. Sending report to Discord... JSON: {Encoding.UTF8.GetString(JsonSerializer.Serialize<object>(webHook))}");
+            ev.Issuer.Broadcast(5, "Thank you for submitting a Report.");
+            foreach (var user in Player.List)
+            {
+                if (user.ReferenceHub.serverRoles.AdminChatPerms)
+                {
+                    user.Broadcast(10, plugin.Config.AcMessage, Broadcast.BroadcastFlags.AdminChat);
+                    if (plugin.Config.VerboseMode)
+                    {
+                        Log.Debug($"Sent Admin Chat alerting to report to: {user.Nickname}");
+                    }
+                }
+            }
+            if (ev.Target.RemoteAdminAccess)
+                Log.Info("Attention - A Staff Member was Reported!");
+            StringContent reportStringContent = new StringContent(Encoding.UTF8.GetString(JsonSerializer.Serialize<object>(webHook)), Encoding.UTF8, "application/json");
+            _ = plugin.HttpHandler.Send(plugin.Config.Webhook, reportStringContent);
+        }
+        public static string StripRichText(string input)
+        {
+            return Regex.Replace(input, "<.*?>", string.Empty);
+        }
+    }
+}
