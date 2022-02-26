630f32d5f2dcf8fa7d6e95d2b5b661f42630ee8c
XoMiya-WPC <inspectorxomiya@gmail.com>
2022-02-26 16:43:41 +0000

    e

--- BetterReporting/Plugin.cs
+++ BetterReporting/Plugin.cs
@@ -20,7 +20,7 @@ namespace BetterReporting
         public override string Prefix { get; } = "Better_Reporting";
         public override Version RequiredExiledVersion { get; } = new Version("4.2.5");
         public static readonly string version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
-        public override Version Version { get; } = new Version("1.3.3");
+        public override Version Version { get; } = new Version("1.3.4");
         internal const string lastModified = "26/02/2022";
 
         public override void OnEnabled() 
